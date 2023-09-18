70715428d6386c8adca38233b709f0cf547a596a
Vedat Kara <vedatkara@mail.com>
2023-09-18 17:13:24 +0300

    Interactive cart. Finish.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
     
     cartSummaryHTML += 
     `
-    <div class="cart-item-container">
+    <div class="js-cart-item-container-${matchedProduct.id} cart-item-container">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -39,7 +39,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="js-delete-link delete-quantity-link link-primary" data-product-id="${matchedProduct.id}">
             Delete
           </span>
         </div>
@@ -95,5 +95,17 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+
+        const itemContainer = document.querySelector(`.js-cart-item-container-${productId}`);
+
+        itemContainer.remove();
+        console.log(cart);
+    });
+});
 
 
